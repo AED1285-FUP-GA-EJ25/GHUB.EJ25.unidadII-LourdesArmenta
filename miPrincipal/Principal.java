2c36552906386b46164338edb913b1759e996d60
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-02-24 17:57:34 +0000

    cambie el mensaje de bienvenida

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -4,8 +4,8 @@ public class Principal {
     
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-        
+        System.out.println("Bienvenidos a nuestra clase de Fundamentos de programacion");
+
         
     }
 }
