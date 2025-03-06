551424a92c140fbe3ba97e25e2abe556328ed57b
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-03-06 17:58:57 +0000

    algunas declaraciones int y short

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -18,7 +18,39 @@ public class Principal {
         autor.saludar();
 
         Usuario usuario = new Usuario(); 
-        usuario.tipoUsuario();
+        int tipo = usuario.tipoUsuario();
+        System.out.println("Esto es lo que regresa tipoUsuario: "+tipo);
+
+        //tipo de datos basicos
+        //Entero
+        int var1; //declara o crea una variable de nombre var1 que puede albergar un int
+        var1 = 5; //le asigno el valoe 5 a la variable var1
+        System.out.println("El valor de la variable var1 = "+var1);
+        var1 = -500;
+        System.out.println("El valor de la variable var1 = "+var1);
+        //var1 = "maria"; no se permite porque var1 esta definido como un int y no puede alberga String
+        var1 = -2147483648;
+        System.out.println("El valor de la variable var1 = "+var1);
+        var1 =  2147483647;
+        System.out.println("El valor de la variable var1 = "+var1);
+        //var1 =  2147483648; no se permite porque sale del rango de una variable tipo int
+
+        short var2; //declarando una varianle de nombre var2 como short
+        var2 = 100;
+        System.out.println("El valor de la variable var2 = "+var2);
+
+        //var2 = 33000;  no se permite porque excede el rango permitido para un short
+        
+
+
+
+
+
+
+
+
+        
+
 
 
         
