b22e281691977f7e4d9fa12be90f57df421fbbe8
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-03-14 17:58:26 +0000

    pendiente termina programa

---
+++ miPrincipal/CalculaSueldoSemanal.java
@@ -0,0 +1,21 @@
+package miPrincipal;
+/*
+ * Proposito: dado el dato del sueldo diario, calcular el sueldo semana, utilizando
+ *    funciones y considerando una constante declarada que indique que la 
+ *    semana tiene 7 dias
+ * Autor:
+ * Fecha
+ */
+import java.util.Scanner;
+public class CalculaSueldoSemanal{
+    public static void main(String[] args) {
+        //definir objeto scanner
+        Scanner entrada = new Scanner(System.in);
+        //definicl constante
+         final byte  SEMANA = 7;
+         
+
+        
+    }
+
+}
